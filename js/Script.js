7be3622729b3bb93373548d59f679961f09e4794
lauragomez-r <laurapaltagomez.est@comfandi.edu.co>
2025-02-06 14:06:16 -0500

    Add files via upload

---
+++ js/Script.js
@@ -0,0 +1,10 @@
+function resposiveMenu(){
+    var x =document.getElementById("nav");
+
+    if(x.className==""){
+        x.className="responsive";
+    }
+    else{
+        x.className="";
+    }
+}
